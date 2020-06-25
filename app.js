01d2a14b645ac0ce83ff05557a9ea588daa383cd
Naomi Isbell <naomiisbell@gmail.com>
2020-06-25 13:35:53 -0400

    added new file

---
+++ app.js
@@ -0,0 +1,26 @@
+// ask if they want to attack or retreat
+// run attack function
+// run math.random 
+
+var player;
+
+function attack() {
+    if()
+}
+
+// USSschwarzenegger object
+// get USSschwarzenegger to attack alienShip
+
+
+let USSschwarzenegger = {
+    hull: 20,
+    firepower: 5,
+    accuracy: 7
+};
+
+// alienShip object
+let AlienShip = {
+    hull: 6,
+    firepower: 3,
+    accuracy: 7
+}
